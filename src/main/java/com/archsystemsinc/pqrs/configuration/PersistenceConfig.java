94b668a172c937253c95d06cf969b410328474cf
grmahun <grmahun1@gmail.com>
2017-07-18 09:50:57 -0400

    Updated code to read the application properties based on the server environment variable (JVM param) spring.profiles.active

--- src/main/java/com/archsystemsinc/pqrs/configuration/PersistenceConfig.java
+++ src/main/java/com/archsystemsinc/pqrs/configuration/PersistenceConfig.java
@@ -4,12 +4,12 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -23,18 +23,43 @@ import com.google.common.base.Preconditions;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource({ "classpath:application.properties" })
 @ComponentScan({ "com.archsystemsinc.pqrs" })
 @EnableJpaRepositories(basePackages = "com.archsystemsinc.pqrs.repository")
 public class PersistenceConfig {
 
-    @Autowired
-    private Environment env;
+	@Value("${jdbc.driverClassName}")
+    String driverClassName;
+	@Value("${jdbc.url}")
+    String jdbcURL;
+	
+	@Value("${jdbc.username}")
+    String jdbcUsername;
+	
+	@Value("${jdbc.password}")
+    String jdbcpassword;
+	
+	@Value("${hibernate.hbm2ddl.auto}")
+    String hbm2ddlAuto;
+	
+	@Value("${hibernate.dialect}")
+    String hibernateDialect;
+	
+	
 
     public PersistenceConfig() {
         super();
     }
+    @Bean
+	public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+		String activeProfile = System.getProperty("spring.profiles.active",	"local");
+		String propertiesFilename = "application-" + activeProfile
+				+ ".properties";
+		System.out.println("propertiesFilename:" + propertiesFilename);
+		PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+		configurer.setLocation(new ClassPathResource(propertiesFilename));
 
+		return configurer;
+	}
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
@@ -52,10 +77,10 @@ public class PersistenceConfig {
     @Bean
     public DataSource dataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(Preconditions.checkNotNull(env.getProperty("jdbc.driverClassName")));
-        dataSource.setUrl(Preconditions.checkNotNull(env.getProperty("jdbc.url")));
-        dataSource.setUsername(Preconditions.checkNotNull(env.getProperty("jdbc.username")));
-        dataSource.setPassword(Preconditions.checkNotNull(env.getProperty("jdbc.password")));
+        dataSource.setDriverClassName(Preconditions.checkNotNull(driverClassName));
+        dataSource.setUrl(Preconditions.checkNotNull(jdbcURL));
+        dataSource.setUsername(Preconditions.checkNotNull(jdbcUsername));
+        dataSource.setPassword(Preconditions.checkNotNull(jdbcpassword));
 
         return dataSource;
     }
@@ -76,9 +101,8 @@ public class PersistenceConfig {
     
     final Properties additionalProperties() {
         final Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-        hibernateProperties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-        // hibernateProperties.setProperty("hibernate.globally_quoted_identifiers", "true");
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", hbm2ddlAuto);
+        hibernateProperties.setProperty("hibernate.dialect", hibernateDialect);
         return hibernateProperties;
     }
 
