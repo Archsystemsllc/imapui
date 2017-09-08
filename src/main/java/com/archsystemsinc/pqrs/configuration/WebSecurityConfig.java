8d8f89cd1939ecabc116ddd89e823cc510174923
fbegum7 <fbegum@archsystemsinc.com>
2017-09-08 09:19:31 -0400

    Logout Functionality

--- src/main/java/com/archsystemsinc/pqrs/configuration/WebSecurityConfig.java
+++ src/main/java/com/archsystemsinc/pqrs/configuration/WebSecurityConfig.java
@@ -31,14 +31,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	http.authorizeRequests()
-        .antMatchers("/user/**").access("hasAuthority('Report Viewer')")
-        .antMatchers("/admin/**").access("hasAuthority('Administrator')")        
-        .and().formLogin().loginPage("/login").successHandler(customSuccessHandler)        
+    	http.authorizeRequests() 
+    	.antMatchers("/user/**").access("hasAuthority('Report Viewer')")
+        .antMatchers("/admin/**").access("hasAuthority('Administrator')")
+        .antMatchers("/resources/**").permitAll()
+        .anyRequest().authenticated()
+        .and().formLogin().loginPage("/login").permitAll().successHandler(customSuccessHandler)        
         .and().csrf()
         .and().exceptionHandling().accessDeniedPage("/Access_Denied")
         .and().logout().logoutSuccessUrl("/login?logout");
-    }
+    } 
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
